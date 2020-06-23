4e9649ab9e23b65b0da1ddb464d71714321ce422
Monje91 <30864186+Monje91@users.noreply.github.com>
2020-06-23 20:31:45 -0300

    Initial commit

---
+++ app/src/main/java/com/example/ppc_tp4_act1/ui/login/LoginFormState.kt
@@ -0,0 +1,8 @@
+package com.example.ppc_tp4_act1.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState(val usernameError: Int? = null,
+                          val passwordError: Int? = null,
+                          val isDataValid: Boolean = false)
